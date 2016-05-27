96a6c0657855edc1946fbe5c08ea916e072c8d15
spacebear5000 <david.giahuy.tu@gmail.com>
2016-05-26 20:20:00 -0400

    add bootstrap things

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
 
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+/* affix the navbar after scroll below header */
+$('#nav').affix({
+      offset: {
+        top: $('header').height()-$('#nav').height()
+      }
+});
+
+/* highlight the top nav as scrolling occurs */
+// $('body').scrollspy({ target: '#nav' })
